ebd6188d681c5bf68ef9fd7807ba4fe14705c2cf
Usman Abubakar <usmanabubakar0014@yahoo.com>
2021-11-17 20:13:29 +0100

    Project Accomplished

--- datasource.js
+++ datasource.js
@@ -3,5 +3,5 @@ const find = require('pouchdb-find')
 
 PouchDB.plugin(find);
 
-const dbUrl = 'https://f42b3b75-48ce-462c-b241-5cf6767080b1-bluemix.cloudantnosqldb.appdomain.cloud/mathfunapi'
+const dbUrl = 'database-url'
 exports.api = new PouchDB(dbUrl, { skip_setup: true })
